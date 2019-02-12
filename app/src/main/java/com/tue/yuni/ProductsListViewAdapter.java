602fac0ca4162675e83abf2acd5d033162637722
Erik9519 <Erik9519@users.noreply.github.com>
2019-02-12 12:34:37 +0100

    Added View More button under reviews. Implemented a simple async image loader to avoid UI freezes. Reworked switching between default product layout and extended product layout.

--- app/src/main/java/com/tue/yuni/ProductsListViewAdapter.java
+++ app/src/main/java/com/tue/yuni/ProductsListViewAdapter.java
@@ -1,14 +1,18 @@
 package com.tue.yuni;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Base64;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewStub;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -52,24 +56,23 @@ public class ProductsListViewAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         // Inflate Layout for each list row
         if (convertView == null) {
-            // Check if the current list Item needs to be a Default or Extended Layout
-            if (position != extendedViewItem)   // Default Layout
-                convertView = LayoutInflater.from(ctx).inflate(R.layout.layout_product, parent, false);
-            else                                // Extended Layout
-                convertView = LayoutInflater.from(ctx).inflate(R.layout.layout_product_extended, parent, false);
+            // Default Layout
+            convertView = LayoutInflater.from(ctx).inflate(R.layout.layout_product, parent, false);
+            // Check if the current list Item needs to the extended view
+            if (position == extendedViewItem) {   // Default Layout
+                LinearLayout extensionLayout = convertView.findViewById(R.id.extendedDetails);
+                extensionLayout.addView(LayoutInflater.from(ctx).inflate(R.layout.layout_product_extended, null,false));
+            }
         } else {
+            LinearLayout extensionLayout = convertView.findViewById(R.id.extendedDetails);
             // Check if the current list Item needs to be a Default or Extended Layout
-            if (position == extendedViewItem) {
-                // If there is no ImageView it means we need to instantiate the Extended Layout
-                if (convertView.findViewById(R.id.productImage) == null)
-                    convertView = LayoutInflater.from(ctx).inflate(R.layout.layout_product_extended, parent, false);
-            } else {
-                // If there is a ImageView it means we need to instantiate the Default Layout
-                if (convertView.findViewById(R.id.productImage) != null)
-                    convertView = LayoutInflater.from(ctx).inflate(R.layout.layout_product, parent, false);
+            if (position == extendedViewItem && extensionLayout.getChildCount() == 0) {
+                extensionLayout.addView(LayoutInflater.from(ctx).inflate(R.layout.layout_product_extended, null,false));
+            } else if (position != extendedViewItem && extensionLayout.getChildCount() > 0){
+                extensionLayout.removeAllViews();
             }
         }
 
@@ -82,15 +85,30 @@ public class ProductsListViewAdapter extends BaseAdapter {
         // Extended View
         if (position == extendedViewItem) {
             // Item Image
-            ((ImageView) convertView.findViewById(R.id.productImage)).setImageResource(products.get(position).Picture);
+            ImageView imageView = convertView.findViewById(R.id.productImage);
+            imageView.setTag(products.get(position).ID);
+            new AsyncImageViewLoader(ctx, products.get(position), imageView).execute();
             // Reviews List
             LinearLayout reviewsContainer = convertView.findViewById(R.id.productReviews);
+            // Check whether or not there is at least 1 review to display
             if (products.get(position).Reviews != null && products.get(position).Reviews.size() > 0) {
+                // Display at most 2 Reviews
                 for (int i = 0; i < Math.min(2, products.get(position).Reviews.size()); i++) {
                     View view = LayoutInflater.from(ctx).inflate(R.layout.layout_product_review, null);
                     ((TextView) view.findViewById(R.id.reviewText)).setText(products.get(position).Reviews.get(i).Text);
                     ((RatingBar) view.findViewById(R.id.reviewRating)).setRating(products.get(position).Reviews.get(i).Rating);
                     reviewsContainer.addView(view);
+                }
+                // Display View More button if there are more than 2 reviews
+                if (products.get(position).Reviews.size() > 2) {
+                    Button viewMoreReviews = new Button(ctx,null, 0, R.style.Widget_AppCompat_Button_Borderless_Colored);
+                    // Focus Settings necessary to keep list item clickable
+                    viewMoreReviews.setFocusable(false);
+                    viewMoreReviews.setFocusableInTouchMode(false);
+                    // Setup Button Text
+                    viewMoreReviews.setText("View more");
+                    // Add button to the layout
+                    reviewsContainer.addView(viewMoreReviews);
                 }
             } else {
                 reviewsContainer.addView(createTextView(ctx, "No Reviews Available!"));
