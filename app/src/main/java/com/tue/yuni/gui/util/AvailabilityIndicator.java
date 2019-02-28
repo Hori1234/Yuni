de71332199b3c3eb10d8289d8528835174f3d926
Erik9519 <Erik9519@users.noreply.github.com>
2019-02-28 13:24:04 +0100

    AvailabilityIndicator custom attributes added.

--- app/src/main/java/com/tue/yuni/gui/util/AvailabilityIndicator.java
+++ app/src/main/java/com/tue/yuni/gui/util/AvailabilityIndicator.java
@@ -1,6 +1,7 @@
 package com.tue.yuni.gui.util;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
@@ -13,6 +14,8 @@ import com.tue.yuni.R;
 public class AvailabilityIndicator extends View {
     private LayerDrawable d;
     private int availability = 0;
+    private int[] threshold = new int[]{25, 50, 100};
+    private int[] color = new int[]{Color.RED, Color.YELLOW, Color.GREEN};
 
     public AvailabilityIndicator(Context context) {
         super(context);
@@ -20,6 +23,19 @@ public class AvailabilityIndicator extends View {
 
     public AvailabilityIndicator(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.AvailabilityIndicator, 0, 0);
+        threshold = new int[3];
+        color = new int[3];
+        try {
+            threshold[0] = a.getInt(R.styleable.AvailabilityIndicator_lowThreshold, 25);
+            threshold[1] = a.getInt(R.styleable.AvailabilityIndicator_mediumThreshold, 50);
+            threshold[2] = a.getInt(R.styleable.AvailabilityIndicator_highThreshold, 100);
+            color[0] = a.getColor(R.styleable.AvailabilityIndicator_lowThresholdColor, Color.argb(255, 205, 23, 23));
+            color[1] = a.getColor(R.styleable.AvailabilityIndicator_mediumThresholdColor, Color.YELLOW);
+            color[2] = a.getColor(R.styleable.AvailabilityIndicator_highThresholdColor, Color.argb(255, 12, 183, 18));
+        } finally {
+            a.recycle();
+        }
     }
 
     public AvailabilityIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -37,14 +53,11 @@ public class AvailabilityIndicator extends View {
             d = (LayerDrawable)getResources().getDrawable(R.drawable.availability_progressbar, null);
         GradientDrawable circle = (GradientDrawable)d.findDrawableByLayerId(R.id.availability);
 
-        if (availability <= 25) {
-            circle.setColor(Color.argb(255, 205, 23, 23));
-        }
-        else if (availability <= 50) {
-            circle.setColor(Color.YELLOW);
-        }
-        else if (availability <= 100) {
-            circle.setColor(Color.argb(255, 12, 183, 18));
+        for (byte i = 0; i < 3; i++){
+            if (availability <= threshold[i])
+                circle.setColor(color[i]);
+            else
+                break;
         }
 
         d.setBounds(0, 0, getWidth(), getHeight());
@@ -65,5 +78,21 @@ public class AvailabilityIndicator extends View {
         if (d == null)
             d = (LayerDrawable)getResources().getDrawable(R.drawable.availability_progressbar, null);
         setMeasuredDimension(d.getIntrinsicWidth(), d.getIntrinsicHeight());
+    }
+
+    public void setThreshold(int index, int threshold) {
+        this.threshold[index] = threshold;
+    }
+
+    public void setColor(int index, int color) {
+        this.color[index] = color;
+    }
+
+    public int getThreshold(int index) {
+        return threshold[index];
+    }
+
+    public int getColor(int index) {
+        return color[index];
     }
 }
