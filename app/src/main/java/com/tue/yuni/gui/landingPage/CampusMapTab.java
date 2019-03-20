684027e76bc452d30b053c8f17ce8b52b723df35
Hori1234 <31578610+Hori1234@users.noreply.github.com>
2019-03-20 16:18:37 +0100

    Addind the AddMenuItem Fragment (#25)
    * Addind the AddMenuItem Fragment
    
    The addMenuItem fragment changes :
    1. androidx -> android.support
    2. using the original libraries
    
    * Small changes:

--- app/src/main/java/com/tue/yuni/gui/landingPage/CampusMapTab.java
+++ app/src/main/java/com/tue/yuni/gui/landingPage/CampusMapTab.java
@@ -1,15 +1,21 @@
 package com.tue.yuni.gui.landingPage;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.tue.yuni.R;
+import com.tue.yuni.gui.addItemToTheListMenu.MenuItemFragment;
 
 public class CampusMapTab extends Fragment {
     @Nullable
@@ -19,9 +25,23 @@ public class CampusMapTab extends Fragment {
         ((Button)view.findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.addToBackStack("LandingPage");
+                ft.replace(R.id.content, new MenuItemFragment());
+                ft.commit();
             }
         });
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 }
