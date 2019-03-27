1dedf48d8ef24511fba2757b8c05dbe17a067fae
Luke van Unen <lukevanunen@live.nl>
2019-03-27 07:19:09 +0100

    fix Availibility dialogue select bug

--- app/src/main/java/com/tue/yuni/gui/editCanteenDialog/availabilityDialog.java
+++ app/src/main/java/com/tue/yuni/gui/editCanteenDialog/availabilityDialog.java
@@ -66,7 +66,7 @@ public class availabilityDialog implements View.OnClickListener{
                 @Override
                 public void onClick(View v) {
                     availabilityGroup.clearCheck();
-                    lowStockButton.toggle();
+                    noStockButton.toggle();
                 }
             });
 
