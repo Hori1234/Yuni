ab61a3b996f233a9b0bf1e453084088c3606d93d
Erik9519 <Erik9519@users.noreply.github.com>
2019-04-03 12:06:04 +0200

    Multiple Bug fixes. Changed LocationService to singleton.

--- app/src/main/java/com/tue/yuni/gui/MainActivity.java
+++ app/src/main/java/com/tue/yuni/gui/MainActivity.java
@@ -2,15 +2,12 @@ package com.tue.yuni.gui;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.os.Vibrator;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.tue.yuni.R;
 import com.tue.yuni.gui.landingPage.LandingView;
@@ -30,16 +27,6 @@ public class MainActivity extends AppCompatActivity {
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                vibrator.vibrate(150);
-            }
-        });
         PasswordStorage.initialize(getApplicationContext());
         // Instantiate Favorite Storage
         FavouriteStorage.initialize(getApplicationContext());
@@ -53,16 +40,19 @@ public class MainActivity extends AppCompatActivity {
             ft.commit();
         }
 
-        LocationService.initialise(
-                this,
-                (loc) -> {
+        LocationService.initialize(this,  (locationService, success) -> {
+            // Location service is initialized
+            if (success) {
+                locationService.requestLocation(location -> {
                     Log.d("abcd", LocationService.getWalkingTime(
                             new Location(5.484207, 51.446439),
-                            loc
+                            location
                     ) + "");
-                }
-        );
-
+                });
+            } else {
+                System.exit(0);
+            }
+        });
 }
 
     @Override
@@ -91,5 +81,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        LocationService.get().onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/com/tue/yuni/services/location/LocationService.java
+++ app/src/main/java/com/tue/yuni/services/location/LocationService.java
@@ -4,17 +4,22 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.tue.yuni.models.Location;
 
 import java.text.DecimalFormat;
 
 public class LocationService {
+    private static LocationService locationService = null;
+
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
     private static final int LATITUDE_CONSTANT = 111200; //=1 degrees, because 0.005959 degrees = 663.63 m
     private static final int LONGITUDE_CONSTANT = 70000; //= 1 degrees, because 0.15668 degrees = 1100 m
@@ -23,6 +28,8 @@ public class LocationService {
     //60: walking time in minutes; 1:time in hours; 3600: time in seconds
     private static FusedLocationProviderClient mFusedLocationClient;
     private static DecimalFormat deci2 = new DecimalFormat(".##");
+    private Activity activity;
+    private LocationInitializedListener parent;
 
     public static double getWalkingTime(Location start, Location end) {
         double deltaLat = Math.abs(start.getLatitude() - end.getLatitude());
@@ -31,15 +38,26 @@ public class LocationService {
         return (deltaLat * LATITUDE_CONSTANT + deltaLon * LONGITUDE_CONSTANT) / WALKING_VELOCITY * WALK_TIME_IN_MINUTES;
     }
 
-    public static void initialise(Activity activity, LocationReceivedListener listener) {
+    public static void initialize(Activity activity, LocationInitializedListener onDone) {
+        if (locationService == null) {
+            synchronized (LocationService.class) {
+                locationService = new LocationService(activity, onDone);
+            }
+        }
+    }
+
+    public static LocationService get(){
+        return locationService;
+    }
+
+    private LocationService(Activity activity, LocationInitializedListener onDone) {
+        this.parent = onDone;
+        this.activity = activity;
         if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            //if permission is not granted
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-
-                new AlertDialog.Builder(activity.getApplicationContext())
+            // Request Permission if not granteed
+                Log.d("abcd", "approved 1");
+                new AlertDialog.Builder(activity)
                         .setTitle("Required Location Permission")
                         .setMessage("You have to give this permission before you can use the app")
                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -52,24 +70,22 @@ public class LocationService {
                         })
                         .setNegativeButton(
                                 "Cancel",
-                                (dialogInterface, i) -> dialogInterface.dismiss())
+                                (dialogInterface, i) -> {
+                                    dialogInterface.dismiss();
+                                    System.exit(0);
+                                })
+                        .setCancelable(false)
                         .create()
                         .show();
-
-
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(activity,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
         } else {
-            // Permission has already been granted
             mFusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
+            onDone.onInitialized(this, true);
+        }
+    }
+
+    public void requestLocation(final LocationReceivedListener listener) {
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
             mFusedLocationClient.getLastLocation().addOnSuccessListener(
                     // Activity,
                     location -> {
@@ -83,7 +99,29 @@ public class LocationService {
                             listener.onLocationReceived(new Location(longitude, latitude));
                         }
                     });
-
+            mFusedLocationClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("LocationService", "Failuire");
+                }
+            });
         }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
+                    parent.onInitialized(this, true);
+                } else {
+                    parent.onInitialized(this, false);
+                }
+                return;
+            }
+        }
+
     }
 }
